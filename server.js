90926e1ab6b965610c5345c288ef2cb74158442a
deepviber0 <deepcoders0@gmail.com>
2024-12-01 15:35:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const app=express()
+const port=8082
+const path=require('path')
+const layout=require('express-ejs-layouts')
+app.use(express.static(path.join(__dirname,"html")))
+app.set("view engine","ejs")
+app.set("views", path.join(__dirname, "html"));
+//app.use(layout)
+app.get('/home',(req,res)=>{
+    try {
+        res.render("home",{user:"umar"}); // Ensure "home.ejs" exists in the views folder.
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred while rendering the page.");
+    }
+})
+
+app.get('/abt',(req,res)=>{
+    res.render("about")
+})
+
+app.listen(port,()=>{
+    console.log(`SERVER IS RUNNING ON ${port}`);   
+})
